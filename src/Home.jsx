a862c0d645b17891109e372165cb36e59cb06dad
Cesar Trujillo <cesar.trujillo@cbqasolutions.com>
2024-09-24 20:25:37 -0600

    Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import './Home.css';
+
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+        setUsers(storedUsers);
+    }, []);
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="user-list-container">
+            <h2>Lista de Registrados</h2>
+            {users.length === 0 ? (
+                <p>No hay usuarios registrados.</p>
+            ) : (
+                <ul className="user-list">
+                    {users.map((user, index) => (
+                        <li key={index} className="user-item">
+                            <strong>Nombre:</strong> {user.nombre} {user.apellidos} <br />
+                            <strong>Correo Electrónico:</strong> {user.email} <br />
+                            <strong>Fecha de Nacimiento:</strong> {user.fechaNacimiento} <br />
+                            <strong>Sexo:</strong> {user.sexo}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
